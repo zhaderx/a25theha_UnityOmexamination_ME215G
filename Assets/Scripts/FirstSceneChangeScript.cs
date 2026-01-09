b9b61f13d610c9a6897d9fb2eaca5b6d6cf670dc
zhaderx <theohanje@gmail.com>
2026-01-09 09:48:18 +0100

    Fixade ett Dialogue system samt en startscene med backstory

---
+++ Assets/Scripts/FirstSceneChangeScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This component can be used to access methods for changing the scene, and quitting the game
+// It can be used together with UnityEventOnTrigger, or UI-button-events, to decide when a scene should be changed or the game should be closed
+public class SceneChnagerFirst : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        // Load the scene named "NewScene"
+        SceneManager.LoadSceneAsync("SecondScene", LoadSceneMode.Single);
+    }
+
+}
